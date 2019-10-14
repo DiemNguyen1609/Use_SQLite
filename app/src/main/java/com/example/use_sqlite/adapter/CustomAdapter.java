f4e302d5d90f6ad1f428a547190e70249b05dec9
Nguyen Thi Ngoc Diem <ngocdiem226@gmail.com>
2019-10-14 10:37:23 +0700

    finsh

--- app/src/main/java/com/example/use_sqlite/adapter/CustomAdapter.java
+++ app/src/main/java/com/example/use_sqlite/adapter/CustomAdapter.java
@@ -1,6 +1,8 @@
 package com.example.use_sqlite.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,66 +37,78 @@ public class CustomAdapter extends ArrayAdapter<Student> {
     public void setCustomButtonListner(customButtonListener listener) {
         this.customListner = listener;
     }*/
-    public CustomAdapter(@NonNull Context context, int resource, @NonNull List<Student> objects,@NonNull DBStudent dbStudent) {
+    public CustomAdapter(@NonNull Context context, int resource, @NonNull List<Student> objects, @NonNull DBStudent dbStudent) {
         super(context, resource, objects);
         this.context = context;
         this.arrayList = objects;
         this.resource = resource;
-        this.dbStudent=dbStudent;
+        this.dbStudent = dbStudent;
     }
 
     @NonNull
     @Override
     public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-       final ViewHolder viewHolder;
-       if(convertView==null)
-       {
-           convertView = LayoutInflater.from(context).inflate(R.layout.row_list_student,parent,false);
-           viewHolder=new ViewHolder();
-           viewHolder.tvId=(TextView)convertView.findViewById(R.id.tv_id);
-           viewHolder.tvName=(TextView)convertView.findViewById(R.id.tv_Name);
-           viewHolder.tvAddress=(TextView)convertView.findViewById(R.id.tv_Address);
-           viewHolder.tvPhoneNumber=(TextView)convertView.findViewById(R.id.tv_Phone);
-           viewHolder.tvEmail=(TextView)convertView.findViewById(R.id.tv_Email);
-           viewHolder.imageViewDelete=(ImageView)convertView.findViewById(R.id.delete);
+        final ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.row_list_student, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.tvId = (TextView) convertView.findViewById(R.id.tv_id);
+            viewHolder.tvName = (TextView) convertView.findViewById(R.id.tv_Name);
+            viewHolder.tvAddress = (TextView) convertView.findViewById(R.id.tv_Address);
+            viewHolder.tvPhoneNumber = (TextView) convertView.findViewById(R.id.tv_Phone);
+            viewHolder.tvEmail = (TextView) convertView.findViewById(R.id.tv_Email);
+            viewHolder.imageViewDelete = (ImageView) convertView.findViewById(R.id.delete);
 
 
-           convertView.setTag(viewHolder);
-       }else{
-           viewHolder=(ViewHolder)convertView.getTag();
-       }
-       final Student student=arrayList.get(position);
-       viewHolder.tvId.setText(student.getID()+"");
-       viewHolder.tvName.setText(student.getName());
-       viewHolder.tvAddress.setText(student.getAddress());
-       viewHolder.tvPhoneNumber.setText(student.getPhone_Number());
-       viewHolder.tvEmail.setText(student.getEmail());
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        final Student student = arrayList.get(position);
+        viewHolder.tvId.setText(student.getID() + "");
+        viewHolder.tvName.setText(student.getName());
+        viewHolder.tvAddress.setText(student.getAddress());
+        viewHolder.tvPhoneNumber.setText(student.getPhone_Number());
+        viewHolder.tvEmail.setText(student.getEmail());
 
-       viewHolder.imageViewDelete.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               String id=viewHolder.tvId.getText()+"";
+        viewHolder.imageViewDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String id = viewHolder.tvId.getText() + "";
                /*if (customListner != null) {
                    customListner.onButtonClickListner(position,id);
                    notifyDataSetChanged();
                }*/
+                AlertDialog.Builder builder=new AlertDialog.Builder(getContext());
+                builder.setTitle("Alert!");
+                builder.setMessage("Do you want to delete this student?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dbStudent.deleteStudent(Integer.valueOf(id));
+                        arrayList.clear();
+                        arrayList.addAll(dbStudent.getAllStudent());
+                        notifyDataSetChanged();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alertDialog=builder.create();
+                alertDialog.show();
 
-               dbStudent.deleteStudent(Integer.valueOf(id));
-               Toast.makeText(getContext(),id+"",Toast.LENGTH_SHORT).show();
-                   arrayList.clear();
-                   arrayList.addAll(dbStudent.getAllStudent());
-                   notifyDataSetChanged();
 
 
-
-
-
-           }
-       });
+            }
+        });
 
 
         return convertView;
     }
+
     public class ViewHolder
     {
         private TextView tvId;
